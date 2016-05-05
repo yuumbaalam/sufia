1c99bb2b2c27e9ef0d230fb429092c29544248b4
E. Lynette Rayle <elr37@cornell.edu>
2016-05-05 15:12:00 -0400

    take 1

--- app/forms/sufia/forms/collection_form.rb
+++ app/forms/sufia/forms/collection_form.rb
@@ -1,5 +1,8 @@
 module Sufia::Forms
   class CollectionForm < CurationConcerns::Forms::CollectionEditForm
+
+    delegate :depositor, to: :model
+
     def rendered_terms
       terms - [:visibility]
     end
