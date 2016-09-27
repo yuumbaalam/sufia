560665fd7133f9a8b26817143c22f6e0cea8c858
Carolyn Cole <cam156@psu.edu>
2016-09-27 07:07:14 -0400

    Fixing up routing tests

--- spec/routing/route_spec.rb
+++ spec/routing/route_spec.rb
@@ -16,12 +16,13 @@ describe 'Routes', type: :routing do
   end
 
   describe "Features" do
+    routes { CurationConcerns::Engine.routes }
     it "routes to the features controller" do
-      expect(get: '/admin/features').to route_to(controller: 'sufia/admin/features', action: 'index')
+      expect(get: '/admin/features').to route_to(controller: 'curation_concerns/admin/features', action: 'index')
     end
 
     it "routes to the strategies controller" do
-      expect(patch: '/admin/features/foo/strategies/bar').to route_to(controller: 'sufia/admin/strategies', action: 'update', id: 'bar', feature_id: 'foo')
+      expect(patch: '/admin/features/foo/strategies/bar').to route_to(controller: 'curation_concerns/admin/strategies', action: 'update', id: 'bar', feature_id: 'foo')
     end
   end
 
@@ -32,11 +33,12 @@ describe 'Routes', type: :routing do
   end
 
   describe 'Admin' do
+    routes { CurationConcerns::Engine.routes }
     it 'routes the admin dashboard' do
-      expect(get: '/admin').to route_to(controller: 'sufia/admin', action: 'show')
+      expect(get: '/admin').to route_to(controller: 'curation_concerns/admin', action: 'index')
     end
     it 'routes the statistics page' do
-      expect(get: '/admin/stats').to route_to(controller: 'sufia/admin/stats', action: 'show')
+      expect(get: '/admin/stats').to route_to(controller: 'curation_concerns/admin/stats', action: 'show')
     end
   end
 
